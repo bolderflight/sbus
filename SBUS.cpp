5aa31f36b1849c0e499eca4511b69ac6e2353d75
Brian Taylor <brian.taylor@bolderflight.com>
2016-04-28 11:20:06 -0500

    Added a readCal function that lets you just get the +/-1 calibrated channel values from SBUS

--- SBUS.cpp
+++ SBUS.cpp
@@ -2,7 +2,7 @@
 // title:     SBUS.cpp
 // author:    Taylor, Brian R.
 // email:     brian.taylor@bolderflight.com
-// date:      YYYY-MM-DD 
+// date:      2016-04-28 
 // license: 
 //
 
@@ -17,6 +17,7 @@ SBUS::SBUS(int bus){
 /* starts the serial communication */
 void SBUS::begin(){
 
+  // initialize parsing state
   _fpos = 0;
 
   // select the serial port
@@ -34,41 +35,71 @@ void SBUS::begin(){
   _port->begin(100000,SERIAL_8N2_RXINV_TXINV);
 }
 
+/* read the SBUS data and calibrate it to +/- 1 */
+bool SBUS::readCal(float* calChannels, uint8_t* failsafe, int* lostFrames){
+  int16_t channels[16];
+
+  // read the SBUS data
+  if(read(&channels[0],failsafe,lostFrames)){
+
+    // linear calibration
+    for(uint8_t i = 0; i < 16; i++){
+      calChannels[i] = channels[i] * SBUS_SCALE + SBUS_BIAS;
+    }
+
+    // return true on receiving a full packet
+    return true;
+  }
+  else{
+
+    // return false if a full packet is not received
+    return false;
+  }
+
+}
+
 /* read the SBUS data */
 bool SBUS::read(int16_t* channels, uint8_t* failsafe, int* lostFrames){
 
+  // parse the SBUS packet
   if(parse()){
-    channels[0]  = (int16_t) ((payload[0]    |payload[1]<<8)                          & 0x07FF);
-    channels[1]  = (int16_t) ((payload[1]>>3 |payload[2]<<5)                          & 0x07FF);
-    channels[2]  = (int16_t) ((payload[2]>>6 |payload[3]<<2 |payload[4]<<10)  & 0x07FF);
-    channels[3]  = (int16_t) ((payload[4]>>1 |payload[5]<<7)                          & 0x07FF);
-    channels[4]  = (int16_t) ((payload[5]>>4 |payload[6]<<4)                          & 0x07FF);
-    channels[5]  = (int16_t) ((payload[6]>>7 |payload[7]<<1 |payload[8]<<9)   & 0x07FF);
-    channels[6]  = (int16_t) ((payload[8]>>2 |payload[9]<<6)                          & 0x07FF);
-    channels[7]  = (int16_t) ((payload[9]>>5 |payload[10]<<3)                         & 0x07FF);
-    channels[8]  = (int16_t) ((payload[11]   |payload[12]<<8)                         & 0x07FF);
-    channels[9]  = (int16_t) ((payload[12]>>3|payload[13]<<5)                         & 0x07FF);
-    channels[10] = (int16_t) ((payload[13]>>6|payload[14]<<2|payload[15]<<10) & 0x07FF);
-    channels[11] = (int16_t) ((payload[15]>>1|payload[16]<<7)                         & 0x07FF);
-    channels[12] = (int16_t) ((payload[16]>>4|payload[17]<<4)                         & 0x07FF);
-    channels[13] = (int16_t) ((payload[17]>>7|payload[18]<<1|payload[19]<<9)  & 0x07FF);
-    channels[14] = (int16_t) ((payload[19]>>2|payload[20]<<6)                         & 0x07FF);
-    channels[15] = (int16_t) ((payload[20]>>5|payload[21]<<3)                         & 0x07FF);
 
+    // 16 channels of 11 bit data
+    channels[0]  = (int16_t) ((_payload[0]    |_payload[1]<<8)                          & 0x07FF);
+    channels[1]  = (int16_t) ((_payload[1]>>3 |_payload[2]<<5)                          & 0x07FF);
+    channels[2]  = (int16_t) ((_payload[2]>>6 |_payload[3]<<2 |_payload[4]<<10)  & 0x07FF);
+    channels[3]  = (int16_t) ((_payload[4]>>1 |_payload[5]<<7)                          & 0x07FF);
+    channels[4]  = (int16_t) ((_payload[5]>>4 |_payload[6]<<4)                          & 0x07FF);
+    channels[5]  = (int16_t) ((_payload[6]>>7 |_payload[7]<<1 |_payload[8]<<9)   & 0x07FF);
+    channels[6]  = (int16_t) ((_payload[8]>>2 |_payload[9]<<6)                          & 0x07FF);
+    channels[7]  = (int16_t) ((_payload[9]>>5 |_payload[10]<<3)                         & 0x07FF);
+    channels[8]  = (int16_t) ((_payload[11]   |_payload[12]<<8)                         & 0x07FF);
+    channels[9]  = (int16_t) ((_payload[12]>>3|_payload[13]<<5)                         & 0x07FF);
+    channels[10] = (int16_t) ((_payload[13]>>6|_payload[14]<<2|_payload[15]<<10) & 0x07FF);
+    channels[11] = (int16_t) ((_payload[15]>>1|_payload[16]<<7)                         & 0x07FF);
+    channels[12] = (int16_t) ((_payload[16]>>4|_payload[17]<<4)                         & 0x07FF);
+    channels[13] = (int16_t) ((_payload[17]>>7|_payload[18]<<1|_payload[19]<<9)  & 0x07FF);
+    channels[14] = (int16_t) ((_payload[19]>>2|_payload[20]<<6)                         & 0x07FF);
+    channels[15] = (int16_t) ((_payload[20]>>5|_payload[21]<<3)                         & 0x07FF);
 
-    if ((payload[22] >> 3) & 0x0001) {
+    // failsafe state
+    if ((_payload[22] >> 3) & 0x0001) {
       *failsafe = 1;
     } else {
       *failsafe = 0;
     }
 
-    if ((payload[23] >> 2) & 0x0001) {
+    // count lost frames
+    if ((_payload[23] >> 2) & 0x0001) {
       *lostFrames = *lostFrames + 1;
     }
 
+    // return true on receiving a full packet
     return true;
   }
   else{
+
+    // return false if a full packet is not received
     return false;
   }
 }
@@ -92,7 +123,7 @@ bool SBUS::parse(){
 
     // strip off the data
     if((_fpos-1) < PAYLOAD_SIZE){
-      payload[_fpos-1] = c;
+      _payload[_fpos-1] = c;
       _fpos++;
     }
 
--- SBUS.h
+++ SBUS.h
@@ -2,7 +2,7 @@
 // title:     SBUS.h
 // author:    Taylor, Brian R.
 // email:     brian.taylor@bolderflight.com
-// date:      YYYY-MM-DD 
+// date:      2016-04-28 
 // license: 
 //
 
@@ -13,17 +13,20 @@
 
 #define SBUS_HEADER 	0xF0
 #define SBUS_FOOTER		0x00
-#define PAYLOAD_SIZE	24				
+#define PAYLOAD_SIZE	24
+#define SBUS_SCALE		0.0012202562538133
+#define SBUS_BIAS		-1.20988407565589				
 
 class SBUS{
   public:
     SBUS(int bus);
     void begin();
     bool read(int16_t* channels, uint8_t* failsafe, int* lostFrames);
+    bool readCal(float* calChannels, uint8_t* failsafe, int* lostFrames);
   private:
   	int _bus;
   	int _fpos;
-  	uint8_t payload[PAYLOAD_SIZE];
+  	uint8_t _payload[PAYLOAD_SIZE];
   	HardwareSerial* _port;
   	bool parse();
 };
